63e18004590b21238052d89673c9f5ecc6d8e041
Shekhov67 <shehs6767@mail.ru>
2024-02-21 17:42:41 +0300

    create user ver 2.1

--- test_firstTest.py
+++ test_firstTest.py
@@ -3,21 +3,28 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import openpyxl
 import pytest
+from openpyxl import Workbook, load_workbook
+
 
 @pytest.fixture()
 def log():
-    print("test")
+    print("testFixture")
     return "value"
 
 def test_first(log):
 
-    book = openpyxl.open('ConnAutoTest.xlsx', read_only=True)
+    book = load_workbook('ConnAutoTest.xlsx')
 
     sheet = book.active
 
-    print(sheet[1][0].value)
+    print(sheet['I3'].value)
+
+    row = sheet.max_row
+
+    print(f'{row} строк')
+
+
 
     driver = webdriver.Chrome()
 
@@ -40,7 +47,8 @@ def test_first(log):
 
         driver.find_element(By.XPATH, "//div[text()=' Администрирование ']").click()
 
-        for i in range(3):
+        for i in range(2, row):
+
             WebDriverWait(driver, 5).until(
                 EC.presence_of_element_located((By.XPATH, "//div[text()='Новый сотрудник']")))
 
@@ -49,15 +57,26 @@ def test_first(log):
             WebDriverWait(driver, 5).until(
                 EC.presence_of_element_located((By.XPATH, "//div[@class='ant-modal-body']")))
 
-            driver.find_element(By.XPATH, "//input[@placeholder='Введите имя сотрудника']").send_keys('Авто')
-            driver.find_element(By.XPATH, "//input[@placeholder='Введите отчество сотрудника']").send_keys('Тестович')
-            driver.find_element(By.XPATH, "//input[@placeholder='Введите фамилию сотрудника']").send_keys('Тестов')
-            driver.find_element(By.XPATH, "//input[@placeholder='Выберите дату']").send_keys('01.02.1990')
-            driver.find_element(By.XPATH, "//input[@placeholder='Должность']").send_keys("Автотестовый агент")
-            driver.find_element(By.XPATH, "(//input[@placeholder='Телефон'])[1]").send_keys('89636027609')
-            driver.find_element(By.XPATH, "(//input[@placeholder='Телефон'])[2]").send_keys('911')
-            driver.find_element(By.XPATH, "//input[@placeholder='Кабинет']").send_keys('112')
-            driver.find_element(By.XPATH, "//input[@placeholder='Введите e-mail']").send_keys(f'{i}autotest@gmail.com')
+            driver.find_element(By.XPATH, "//input[@placeholder='Введите имя сотрудника']").send_keys(
+                sheet[f'B{i}'].value)
+            driver.find_element(By.XPATH, "//input[@placeholder='Введите отчество сотрудника']").send_keys(
+                sheet[f'C{i}'].value)
+            driver.find_element(By.XPATH, "//input[@placeholder='Введите фамилию сотрудника']").send_keys(
+                sheet[f'A{i}'].value)
+            time.sleep(4)
+            #Выводится не правильный формат даты из экселя
+            driver.find_element(By.XPATH, "//input[@placeholder='Выберите дату']").send_keys(
+                sheet[f'I{i}'].value)
+            driver.find_element(By.XPATH, "//input[@placeholder='Должность']").send_keys(
+                sheet[f'E{i}'].value)
+            driver.find_element(By.XPATH, "(//input[@placeholder='Телефон'])[1]").send_keys(
+                sheet[f'G{i}'].value)
+            driver.find_element(By.XPATH, "(//input[@placeholder='Телефон'])[2]").send_keys(
+                sheet[f'F{i}'].value)
+            driver.find_element(By.XPATH, "//input[@placeholder='Кабинет']").send_keys(
+                sheet[f'H{i}'].value)
+            driver.find_element(By.XPATH, "//input[@placeholder='Введите e-mail']").send_keys(
+                sheet[f'D{i}'].value)
             driver.find_element(By.XPATH, "//input[@placeholder='Пароль']").send_keys('111111')
             driver.find_element(By.XPATH, "//input[@placeholder='Подтверждение пароля']").send_keys('111111')
             time.sleep(1)
