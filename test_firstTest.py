2cac847d17f370f1e00fcabf5b5fd5e85f9a19aa
Shekhov67 <shehs6767@mail.ru>
2024-02-21 15:15:07 +0300

    create user ver 2.0

--- test_firstTest.py
+++ test_firstTest.py
@@ -40,28 +40,31 @@ def test_first(log):
 
         driver.find_element(By.XPATH, "//div[text()=' Администрирование ']").click()
 
-        WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//div[text()='Новый сотрудник']")))
+        for i in range(3):
+            WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.XPATH, "//div[text()='Новый сотрудник']")))
 
-        driver.find_element(By.XPATH, "//div[text()='Новый сотрудник']").click()
+            driver.find_element(By.XPATH, "//div[text()='Новый сотрудник']").click()
 
-        WebDriverWait(driver, 5).until(
-                 EC.presence_of_element_located((By.XPATH, "//div[@class='ant-modal-body']")))
+            WebDriverWait(driver, 5).until(
+                EC.presence_of_element_located((By.XPATH, "//div[@class='ant-modal-body']")))
 
-        driver.find_element(By.XPATH, "//input[@placeholder='Введите имя сотрудника']").send_keys('Авто')
-        driver.find_element(By.XPATH, "//input[@placeholder='Введите отчество сотрудника']").send_keys('Тестович')
-        driver.find_element(By.XPATH, "//input[@placeholder='Введите фамилию сотрудника']").send_keys('Тестов')
-        driver.find_element(By.XPATH, "//input[@placeholder='Выберите дату']").send_keys('01.02.1990')
-        driver.find_element(By.XPATH, "//input[@placeholder='Должность']").send_keys("Автотестовый агент")
-        driver.find_element(By.XPATH, "(//input[@placeholder='Телефон'])[1]").send_keys('89636027609')
-        driver.find_element(By.XPATH, "(//input[@placeholder='Телефон'])[2]").send_keys('911')
-        driver.find_element(By.XPATH, "//input[@placeholder='Кабинет']").send_keys('112')
-        driver.find_element(By.XPATH, "//input[@placeholder='Введите e-mail']").send_keys('autotest01@gmail.com')
-        driver.find_element(By.XPATH, "//input[@placeholder='Пароль']").send_keys('111111')
-        driver.find_element(By.XPATH, "//input[@placeholder='Подтверждение пароля']").send_keys('111111')
-        time.sleep(5)
-        driver.find_element(By.XPATH, "(//button[@class='ant-btn ant-btn-primary'])[2]").click()
-        time.sleep(5)
+            driver.find_element(By.XPATH, "//input[@placeholder='Введите имя сотрудника']").send_keys('Авто')
+            driver.find_element(By.XPATH, "//input[@placeholder='Введите отчество сотрудника']").send_keys('Тестович')
+            driver.find_element(By.XPATH, "//input[@placeholder='Введите фамилию сотрудника']").send_keys('Тестов')
+            driver.find_element(By.XPATH, "//input[@placeholder='Выберите дату']").send_keys('01.02.1990')
+            driver.find_element(By.XPATH, "//input[@placeholder='Должность']").send_keys("Автотестовый агент")
+            driver.find_element(By.XPATH, "(//input[@placeholder='Телефон'])[1]").send_keys('89636027609')
+            driver.find_element(By.XPATH, "(//input[@placeholder='Телефон'])[2]").send_keys('911')
+            driver.find_element(By.XPATH, "//input[@placeholder='Кабинет']").send_keys('112')
+            driver.find_element(By.XPATH, "//input[@placeholder='Введите e-mail']").send_keys(f'{i}autotest@gmail.com')
+            driver.find_element(By.XPATH, "//input[@placeholder='Пароль']").send_keys('111111')
+            driver.find_element(By.XPATH, "//input[@placeholder='Подтверждение пароля']").send_keys('111111')
+            time.sleep(1)
+            driver.find_element(By.XPATH, "(//button[@class='ant-btn ant-btn-primary'])[2]").click()
+            time.sleep(3)
+
+
     except:
         print('ERROR')
     finally:
