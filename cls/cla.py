9217aa7d8d7b1e69b98dac5bdcbfc8145a06810a
Shekhov67 <shehs6767@mail.ru>
2024-02-28 22:36:31 +0300

    create user ver 2.6

--- cls/cla.py
+++ cls/cla.py
@@ -1,14 +1,18 @@
 import json
-class Car:
+
+class Cars:
     def __init__(self, file_path):
         self.file_path = file_path
         self.file_data = self.read_data()
         self.mark = self.file_data['Mark']
         self.max_speed = self.file_data['max_speed']
+
     def read_data(self):
         with open(self.file_path, 'r') as cars_file:
             return json.load(cars_file)
 
 
-car = Car('cls/cars.json')
+car = Cars('cars.json')
 print(car.file_data)
+print(car.mark)
+print(car.max_speed)
