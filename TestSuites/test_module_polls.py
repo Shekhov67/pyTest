22a261a36d9b64ca06a05b0f27256400b99fb6da
Shekhov67 <shehs6767@mail.ru>
2024-09-30 11:35:07 +0300

    update poll

--- TestSuites/test_module_polls.py
+++ TestSuites/test_module_polls.py
@@ -1,4 +1,4 @@
 import pytest
 def test_suite_polls():
-    pytest.main(['class/test_create_polls_class.py'])
-    pytest.main(['class/test_compl_polls_class.py'])
+    pytest.main(['Polls/class/test_create_polls_class.py'])
+    pytest.main(['Polls/class/test_compl_polls_class.py'])
