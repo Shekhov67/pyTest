216f7f5aa02ecd601b5f08f6b9a6eb3357c6c5da
Shekhov67 <shehs6767@mail.ru>
2024-12-15 12:23:47 +0300

    update TestSuite

--- TestSuites/test_module.py
+++ TestSuites/test_module.py
@@ -3,8 +3,10 @@ import pytest
 # количество циклов прохождения опроса
 num_polls = 23
 
+#Выбрать портал для текста
 url = 'https://staging.connectable.site/'
 #url = 'https://connectable.site/'
+#url = 'https://intranetable.team/'
 
 def test_suite_polls():
     pytest.main(['C:\\Users\\shehs\\PycharmProjects\\pyTest\\Structure\\create_departament_cls.py'])
