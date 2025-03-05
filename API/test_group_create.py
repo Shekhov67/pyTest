b49efebeb61802403a52ca447944e7061e021f71
Shekhov67 <shehs6767@mail.ru>
2025-03-05 21:02:23 +0300

    update

---
+++ API/test_group_create.py
@@ -0,0 +1,32 @@
+import requests
+from API.key_api import header
+
+for i in range(0, 3):
+
+    if i == 0:
+        type_group = 'ОТКРЫТОЙ'
+    elif i == 1:
+        type_group = 'ЗАКРЫТОЙ'
+    elif i == 2:
+        type_group = 'ПРИВАТНОЙ'
+
+
+    API_URL = 'https://staging.connectable.site/api/group/'
+
+    data_group = {
+        'client_id': "api",
+        'creator': "67be00a0e6e983001ee2847c",
+        'creatorId': "67be00a0e6e983001ee2847c",
+        'description': f"Описание {type_group} группы API",
+        'emailSend': False,
+        'lang': "ru",
+        'name': f"Название {type_group} группы API",
+        'type': f'{i}',
+        'userEmail': "t2@gmail.com"
+    }
+
+    response_create_group_open = requests.post(API_URL, headers=header, json=data_group)
+
+    def test_create_open_group():
+        assert response_create_group_open.status_code == 201
+        print(f'код ответа: {response_create_group_open}')
