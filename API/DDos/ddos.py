6c9c2bae645f8dc16384969e69017e0612a5fd4a
Shekhov67 <shehs6767@mail.ru>
2025-03-20 16:19:07 +0300

    update

--- API/DDos/ddos.py
+++ API/DDos/ddos.py
@@ -1,16 +1,43 @@
 import requests
 from API.key_api import header
+import asyncio
 
-API_URL = 'https://staging.connectable.site/api/loginPage'
+API_URL = 'https://staging.connectable.site/api/group/'
 
 data = {
-    'email': "t2@gmail.com",
+    'client_id': "ttu",
+    'creator': "67c6c81d4cde2e001ed4126d",
+    'creatorId': "67c6c81d4cde2e001ed4126d",
+    'description': f"Описание DDoS группы API",
+    'emailSend': False,
     'lang': "ru",
-    'login': "",
-    'password': "111111",
-    'workspace': "ttu"
+    'name': f"Название DDoS группы API",
+    'type': f'1',
+    'userEmail': "t2@gmail.com"
 }
+async def main():
+    for i in range(10):
+        response1 = requests.post(API_URL, headers=header, json=data)
+        response2 = requests.post(API_URL, headers=header, json=data)
+        response3 = requests.post(API_URL, headers=header, json=data)
+        response4 = requests.post(API_URL, headers=header, json=data)
+        response5 = requests.post(API_URL, headers=header, json=data)
+        response6 = requests.post(API_URL, headers=header, json=data)
+        response7 = requests.post(API_URL, headers=header, json=data)
+        response8 = requests.post(API_URL, headers=header, json=data)
+        response9 = requests.post(API_URL, headers=header, json=data)
+        response10 = requests.post(API_URL, headers=header, json=data)
+        response11 = requests.post(API_URL, headers=header, json=data)
+        response12 = requests.post(API_URL, headers=header, json=data)
+        response13 = requests.post(API_URL, headers=header, json=data)
+        response14 = requests.post(API_URL, headers=header, json=data)
+        response15 = requests.post(API_URL, headers=header, json=data)
+        response16 = requests.post(API_URL, headers=header, json=data)
+        response17 = requests.post(API_URL, headers=header, json=data)
+        response18 = requests.post(API_URL, headers=header, json=data)
+        response19 = requests.post(API_URL, headers=header, json=data)
+        response20 = requests.post(API_URL, headers=header, json=data)
 
-for i in range(10):
-    response = requests.post(API_URL, headers=header, json=data)
-    print(response.status_code)
+        print(response20.status_code)
+
+asyncio.run(main())
