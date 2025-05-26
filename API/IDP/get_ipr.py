0546c3d276ee098b63fcc5058676ccf2adddac49
Shekhov67 <shehs6767@mail.ru>
2025-05-26 12:54:13 +0300

    update 26/05/2025

--- API/IDP/get_ipr.py
+++ API/IDP/get_ipr.py
@@ -8,9 +8,14 @@ API_URL = 'https://staging.connectable.site/api/idp/list/682585ca0ac353001e5be71
 response = requests.get(API_URL, headers=header)
 
 body = response.json()
-body_id = body['result'][0]['_id']
+print(body)
 
-print(body_id)
+if body == {'result': []}:
+    print('Нет ИПР')
+else:
+    body_id = body['result'][0]['_id']
+
+    print(body_id)
 
 # Выводим все _id из элементов result
 # for item in data["result"]:
